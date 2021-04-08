47cf0aa164d85a5d5252c3f0823a26e74ea5e589
Manvir SIngh <er.manvirsingh98@gmail.com>
2021-04-08 11:31:25 -0400

    5. Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import GlobalStyle from "./components/GlobalStyle";
 import AboutUs from "./pages/AboutUs";
 
 
 function App() {
   return (
     <div className="app">
+      <GlobalStyle />
      <AboutUs />
     </div>
   );
